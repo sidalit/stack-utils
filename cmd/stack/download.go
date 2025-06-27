b56c697565cd01a7d132690f8a4921aa00cba4b2
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-06-27 10:19:37 +0200

    Improve component installation logging (#66)
    * Add component-specific logging
    
    * Add progress info, handle timeouts

--- cmd/stack/download.go
+++ cmd/stack/download.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/canonical/go-snapctl"
 	"github.com/canonical/stack-utils/pkg/types"
 )
 
-func downloadRequiredComponents() {
-	fmt.Println("Downloading required components ...")
+const (
+	snapdUnknownSnapError = "cannot install components for a snap that is unknown to the store"
+	snapdTimeoutError     = "timeout exceeded while waiting for response"
+)
 
+func downloadRequiredComponents() {
 	// get stack snap option
 	stackName, err := snapctl.Get("stack").Run()
 	if err != nil {
@@ -38,17 +43,35 @@ func downloadRequiredComponents() {
 	}
 
 	// install components
+	// Messages presented to the user should use the term "download" for snapctl install +component.
 	for _, component := range stack.Components {
+		stopProgress := startProgressDots("Downloading " + component + " ")
 		err = snapctl.InstallComponents(component).Run()
+		stopProgress()
 		if err != nil {
-			if strings.Contains(err.Error(), "cannot install components for a snap that is unknown to the store") {
-				fmt.Printf("Skip component installation. Install a local build: sudo snap install <path to %s component>\n", component)
+			if strings.Contains(err.Error(), snapdUnknownSnapError) {
+				fmt.Printf("Error: snap not known to the store. Install a local build of component: %s\n", component)
+				continue
+			} else if strings.Contains(err.Error(), snapdTimeoutError) {
+				fmt.Printf("Error: timeout exceeded while waiting for download of %s\n", component)
+				fmt.Println("Please monitor the progress using the 'snap changes' command and continue when the component installation is complete.")
+				os.Exit(1)
 			} else if strings.Contains(err.Error(), "already installed") {
-				fmt.Println("Skip component installation: already installed:", component)
+				continue
 			} else {
-				fmt.Println("Error installing component:", err)
+				fmt.Printf("Error downloading component: %s: %s\n", component, err)
 				os.Exit(1)
 			}
 		}
+		fmt.Println("Downloaded " + component)
 	}
+}
+
+func startProgressDots(prefix string) (stop func()) {
+	dots := []string{".", "..", "..."}
+	s := spinner.New(dots, time.Second)
+	s.Prefix = prefix
+	s.Start()
+
+	return s.Stop
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/canonical/stack-utils
 go 1.24
 
 require (
+	github.com/briandowns/spinner v1.23.2
 	github.com/canonical/go-snapctl v1.0.0-beta.2
 	github.com/fatih/color v1.18.0
 	github.com/jaypipes/pcidb v1.0.1
@@ -14,4 +15,5 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
 )
